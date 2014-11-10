7f3fafac3d6bee6db372a75f14586b8e90fb7351
Anton Eriksson <antoneri@gmail.com>
2014-11-11 00:16:55 +0100

    Major refactoring

---
+++ umutemp.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+from urllib.request import urlopen
+from html import unescape
+import xml.etree.ElementTree as ET
+
+URL = "http://www8.tfe.umu.se/WeatherWebService2012/Service.asmx/Aktuellavarden"
+NS = "{http://tempuri.org/}"
+
+def fetch_data():
+    try:
+        src = urlopen(URL).read().decode('utf-8')
+    except Exception as e:
+        print("Error: Something went wrong fetching the page: {}".format(e))
+        return None
+
+    xml = unescape(src)
+    root = ET.fromstring(xml)[0]
+
+    def tag_contents(tag):
+        try:
+            return root.find("{}{}".format(NS, tag)).text.strip()
+        except Exception as e:
+            print("Error: Could not find tag {}.".format(tag))
+            return None
+
+    temp = tag_contents("tempmed")
+    speed = tag_contents("vindh")
+    words = tag_contents("vindord")
+
+    temp = "{} °C".format(temp)
+    speed = "{} m/s".format(speed)
+    return { 'temp': temp, 'wind': { 'speed': speed, 'words': words} }
+
+
+def main():
+    import os
+    data = fetch_data()
+    command = 'growlnotify -n Temperatur -t "{}" -m "{} ({})"'.format(
+            data['temp'], data['wind']['words'], data['wind']['speed'])
+    os.system(command)
+
+
+if __name__ == '__main__':
+    main()
