a4230076164c8bebe0552f6fbbd9cb120a0eec0a
Anton Eriksson <antoneri@gmail.com>
2014-11-11 00:45:11 +0100

    Removed the try/except clauses. If it breaks -- it breaks hard!

--- umutemp.py
+++ umutemp.py
@@ -7,21 +7,12 @@ URL = "http://www8.tfe.umu.se/WeatherWebService2012/Service.asmx/Aktuellavarden"
 NS = "{http://tempuri.org/}"
 
 def fetch_data():
-    try:
-        src = urlopen(URL).read().decode('utf-8')
-    except Exception as e:
-        print("Error: Something went wrong fetching the page: {}".format(e))
-        return None
-
+    src = urlopen(URL).read().decode('utf-8')
     xml = unescape(src)
     root = ET.fromstring(xml)[0]
 
     def tag_contents(tag):
-        try:
-            return root.find("{}{}".format(NS, tag)).text.strip()
-        except Exception as e:
-            print("Error: Could not find tag {}.".format(tag))
-            return None
+        return root.find("{}{}".format(NS, tag)).text.strip()
 
     temp = tag_contents("tempmed")
     speed = tag_contents("vindh")
